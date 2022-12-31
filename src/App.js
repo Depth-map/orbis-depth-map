b0a2d15307e996fa5b90bfc93c1a8b57314ef6eb
anon <a@a.com>
2023-01-01 03:19:02 +0530

    many

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import { Orbis } from "@orbisclub/orbis-sdk";
+import LitJsSdk from "@lit-protocol/sdk-nodejs";
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Utilizing Lit Action and PKP, when a user followingor is followers on a SNS application, the data is signed and stored.
+
+And here is where it gets unique.
+
+Count the number of times a user is connected (i.e. Multuals) with the same DID or walletAddress and different applications.
+I believe that by doing so, the depth of the relationship will show up as a graph.
+
+The use case for this graph could be leveraged for Proof of personhood and voting.
+
+Proof of personhood:
+
+A deep connection with the same person could prove that they are human, and the more Mutuals, the more likely the person is human.
+
+Vote: 
+A close relationship could lead to collusion or the same ideology. Reduce the power of voting among those who are close.        </p>
+        
       </header>
     </div>
   );
